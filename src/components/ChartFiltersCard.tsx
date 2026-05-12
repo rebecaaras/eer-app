a3a848c31b0b86b5706725f07e442a75e6a8310f
Rebeca Araújo <rebecaaraujo2013@gmail.com>
2026-05-12 16:39:09 -0300

    add multi-select for ref area input selector

--- src/components/ChartFiltersCard.tsx
+++ src/components/ChartFiltersCard.tsx
@@ -1,55 +1,25 @@
-import { Button } from "../components/ui/button"
-import { Input } from "../components/ui/input"
-import { Label } from "../components/ui/label"
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,} from "../components/ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle,} from "../components/ui/card"
+import {
+  Field,
+  FieldLabel,
+} from "../components/ui/field"
+import MultiSelect from "./ui/multi-select"
 
 
 export default function ChartFiltersCard() {
   return (
     <Card className="w-full max-w-[20%] h-full mr-5 max-h-[600px]">
       <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
+        <CardTitle>Filters</CardTitle>
         <CardDescription>
-          Enter your email below to login to your account
+          Choose which series to display
         </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
       </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
-          </div>
-        </form>
-      </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
+        <Field className="max-w-sm">
+        <FieldLabel htmlFor="inline-start-input">Reference Area</FieldLabel>
+        <MultiSelect/>
+      </Field>
       </CardFooter>
     </Card>
   )
