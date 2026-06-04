a7b51cdcb2490bea1e3feab7370f832588682cc5
Rebeca Araújo <rebecaaraujo2013@gmail.com>
2026-06-04 18:18:02 -0300

    move api call to shared apiContext.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,28 @@
 import './App.css'
 import { ExchangeRateChart } from './components/ExchangeRateChart';
-import Navbar from './components/NavBar';
 import ChartFiltersCard from './components/ChartFiltersCard';
-import { useEffect, useState } from 'react';
-import type { Series } from './types';
+import { ApiContext } from './context/apiContext';
+import { useContext } from 'react';
 
-function App() {
-  const [series, setSeries] = useState<Series[] | undefined>([]);
+export default function App() {
+  const {seriesData, isLoading} = useContext(ApiContext);
 
-  useEffect(() => {
-    fetchSeriesData();
-  }, []);
-  
-  const fetchSeriesData = async () => {
-    try {
-      const response = await fetch('http://127.0.0.1:3000/series');
-      const data = await response.json();
-      setSeries(data)
-      console.log(data)
-    } catch (error) {
-      console.error("seriesData couldn't be fetched.", error)
-    }
+  if (isLoading) {
+    return(
+      <div className="flex flex-col h-screen">
+        <div className='flex flex-row p-8 h-full'>
+          <p>Loading...</p>
+        </div>
+      </div>
+    )
   }
-
+  
   return (
-  <div className="flex flex-col h-screen">
-    <Navbar/>
-    {series && series.length > 0 &&  (
+    <div className="flex flex-col h-screen">
       <div className="flex flex-row p-8 h-full">
-        <ChartFiltersCard data={series}/>
+        <ChartFiltersCard data={seriesData}/>
         <ExchangeRateChart />
       </div>
-    )} 
-  </div>  
+    </div>
   )
 }
-
-export default App;
