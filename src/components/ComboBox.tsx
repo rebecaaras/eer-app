c65c702625eadac8bc77017909fc6f9e7b307e19
Rebeca Araújo <rebecaaraujo2013@gmail.com>
2026-06-27 05:40:53 -0300

    add function onChange passed in ComboBox as a prop so that it exposes the value selected to the above component

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Combobox,
   ComboboxContent,
@@ -10,11 +11,23 @@ import {
 type ComboBoxProps = {
   options: string[];
   placeholder?: string;
+  onChange?: (value: string) => void;
 };
 
-export function ComboBox({options, placeholder = "Select an option"}: ComboBoxProps) {
+export function ComboBox({ options, placeholder = "Select an option", onChange}: ComboBoxProps) {
+  const [value, setValue] = useState("")
+
+  function handleChange(value: string){
+    setValue(value)
+    onChange?.(value)
+  }
+
   return (
-    <Combobox items={options}>
+    <Combobox 
+      items={options}
+      value={value}
+      onValueChange={handleChange}
+    >
       <ComboboxInput placeholder={placeholder} />
       <ComboboxContent>
         <ComboboxEmpty>No items found.</ComboboxEmpty>
